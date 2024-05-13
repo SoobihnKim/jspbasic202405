a2195a1b400a6aeee3a1c482b5d9f6f91b6b92c3
soo <ksbsj92@naver.com>
2024-05-13 12:49:53 +0900

    # MVC architecture part5(delete)

---
+++ src/main/resources/jsp.sql
@@ -0,0 +1,25 @@
+use spring5;
+
+select now() from dual;
+
+CREATE TABLE tbl_person (
+                            id INT(6) PRIMARY KEY,
+                            person_name VARCHAR(255) NOT NULL,
+                            person_age INT(3)
+);
+
+SELECT * FROM tbl_person
+ORDER BY id DESC;
+
+INSERT INTO tbl_person
+VALUES (888, '호호', 11);
+
+drop table tbl_dancer;
+CREATE TABLE tbl_dancer (
+                            id INT(8) PRIMARY KEY auto_increment,
+                            name VARCHAR(255),
+                            crew_name VARCHAR(255),
+                            dance_level VARCHAR(50)
+);
+
+SELECT * FROM tbl_dancer;
